7ed5610173ad562143765febdb6e0ac0f53dce85
Michael Bock <me@thought2.de>
2021-05-18 08:14:48 +0200

    Add functor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /** @since 1.0.0 */
 
+import * as A from 'fp-ts/ReadonlyArray';
+
 // -----------------------------------------------------------------------------
 // Model
 // -----------------------------------------------------------------------------
@@ -69,6 +71,20 @@ export const vec2 = <T>(x: T, y: T): Vec<2, T> => [x, y] as Vec<2, T>;
 export const vec3 = <T>(x: T, y: T, z: T): Vec<3, T> => [x, y, z] as Vec<3, T>;
 
 // -----------------------------------------------------------------------------
+// Functor
+// -----------------------------------------------------------------------------
+
+/**
+ * Maps over a Vec's inner type
+ *
+ * @since 0.1.0
+ * @category Functor
+ */
+export const map = <T1, T2>(f: (x: T1) => T2) => <N extends number>(
+  vec: Vec<N, T1>
+): Vec<N, T2> => A.map(f)(vec as ReadonlyArray<T1>) as Vec<N, T2>;
+
+// -----------------------------------------------------------------------------
 // Internal
 // -----------------------------------------------------------------------------
 
@@ -88,3 +104,9 @@ type OnlyAs<X, T> = T extends X ? T : never;
 type Succ<N extends number> = Add<N, 1>;
 
 type G<N extends number> = Add<1, N>;
+
+// const overArray = <T1, T2>(f: (xs: ReadonlyArray<T1>) => ReadonlyArray<T2>) => <
+//   N extends number
+// >(
+//   vec: Vec<N, T1>
+// ): Vec<N, T2> => f(vec as ReadonlyArray<T1>) as Vec<N, T2>;
--- tests/index.ts
+++ tests/index.ts
@@ -1,53 +1,70 @@
-import * as $ from '../src';
-import * as fc from 'fast-check';
-import { pipe } from 'fp-ts/function';
+import * as $ from '../src'
+import * as fc from 'fast-check'
+import { pipe } from 'fp-ts/function'
 
-describe('empty', () => {
-  it('creates an empty array', () => {
-    expect($.empty()).toStrictEqual([]);
-  });
-});
+describe('Constructors', () => {
+  describe('empty', () => {
+    it('creates an empty array', () => {
+      expect($.empty()).toStrictEqual([])
+    })
+  })
 
-describe('prepend', () => {
-  it('prepends at the beginning', () => {
-    fc.property(fc.string(), fc.string(), (x1, x2) => {
-      expect(
-        pipe($.empty<string>(), $.prepend(x1), $.prepend(x2))
-      ).toStrictEqual([x2, x1]);
-    });
-  });
-});
+  describe('prepend', () => {
+    it('prepends at the beginning', () => {
+      fc.property(fc.string(), fc.string(), (x1, x2) => {
+        expect(
+          pipe($.empty<string>(), $.prepend(x1), $.prepend(x2))
+        ).toStrictEqual([x2, x1])
+      })
+    })
+  })
 
-describe('append', () => {
-  it('appends at the end', () => {
-    fc.property(fc.string(), fc.string(), (x1, x2) => {
-      expect(
-        pipe($.empty<string>(), $.append(x1), $.append(x2))
-      ).toStrictEqual([x1, x2]);
-    });
-  });
-});
+  describe('append', () => {
+    it('appends at the end', () => {
+      fc.property(fc.string(), fc.string(), (x1, x2) => {
+        expect(
+          pipe($.empty<string>(), $.append(x1), $.append(x2))
+        ).toStrictEqual([x1, x2])
+      })
+    })
+  })
 
-describe('singleton', () => {
-  it('creates array of length 1', () => {
-    fc.property(fc.string(), (x1) => {
-      expect(pipe($.singleton(x1))).toStrictEqual([x1]);
-    });
-  });
-});
+  describe('singleton', () => {
+    it('creates array of length 1', () => {
+      fc.property(fc.string(), (x1) => {
+        expect(pipe($.singleton(x1))).toStrictEqual([x1])
+      })
+    })
+  })
 
-describe('vec2', () => {
-  it('creates array of length 2', () => {
-    fc.property(fc.string(), fc.string(), (x1, x2) => {
-      expect(pipe($.vec2(x1, x2))).toStrictEqual([x1, x2]);
-    });
-  });
-});
+  describe('vec2', () => {
+    it('creates array of length 2', () => {
+      fc.property(fc.string(), fc.string(), (x1, x2) => {
+        expect(pipe($.vec2(x1, x2))).toStrictEqual([x1, x2])
+      })
+    })
+  })
 
-describe('vec3', () => {
-  it('creates array of length 3', () => {
-    fc.property(fc.string(), fc.string(), fc.string(), (x1, x2, x3) => {
-      expect(pipe($.vec2(x1, x2))).toStrictEqual([x1, x2, x3]);
-    });
-  });
-});
+  describe('vec3', () => {
+    it('creates array of length 3', () => {
+      fc.property(fc.string(), fc.string(), fc.string(), (x1, x2, x3) => {
+        expect(pipe($.vec3(x1, x2, x3))).toStrictEqual([x1, x2, x3])
+      })
+    })
+  })
+})
+
+describe('Functor', () => {
+  describe('map', () => {
+    it('maps over all fields', () => {
+      fc.property(fc.float(), fc.float(), fc.float(), (x1, x2, x3) => {
+        expect(
+          pipe(
+            $.vec3(x1, x2, x3),
+            $.map((x) => x.toString())
+          )
+        ).toStrictEqual([x1.toString(), x2.toString(), x3.toString()])
+      })
+    })
+  })
+})
